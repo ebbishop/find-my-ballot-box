403e913eea43d617c7f5d62f3fd550c0925ccc2e
Emma Bishop <emma.bishop@temboinc.com>
2020-05-30 23:07:29 -0400

    Initial ballot box finder for Philly

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,144 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <h1>Nearest ballot drop-box</h1>
+    <div class="row d-flex justify-content-center">
+      <gmap-autocomplete class="introInput"
+        @place_changed="placeChanged"
+      ></gmap-autocomplete>
+      <button class="btn btn-light ml-2" @click="clearHome">Clear</button>
+    </div>
+    <div class="row mt-4">
+      <gmap-map
+        :center="center"
+        :zoom="11"
+        style="width:100%;  height: 400px;"
+      >
+        <gmap-info-window
+          v-if="clicked"
+          :position="clicked"
+          :options="{content: clicked.name}"
+        ></gmap-info-window>
+        <gmap-marker
+          v-if="home"
+          :position="home"
+        ></gmap-marker>
+        <gmap-marker
+          :key="index"
+          v-for="(loc, index) in locations"
+          :position="loc"
+          :clickable="true"
+          v-on:click="showTooltip(loc)"
+        ></gmap-marker>
+      </gmap-map>
+    </div>
+    <div class="row mt-4">
+      <table class="text-left">
+        <thead>
+          <td>Name</td>
+          <td>Hours</td>
+          <td>Distance</td>
+        </thead>
+        <tbody>
+          <tr v-for="row in locations" :key="row.name">
+            <td>{{row.name}}</td>
+            <td v-html="row.notes"></td>
+            <td>{{row.distance ? row.distance + ' miles' : ''}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
+import Papa from 'papaparse';
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String,
   },
+  data() {
+    return {
+      home: null,
+      center: {
+        name: 'Philadelphia City Hall',
+        lat: 39.9528041,
+        lng: -75.165672,
+        notes: 'A 24/7 dropbox is located at the South Broad entrance, across the street from the Capital Grille.',
+      },
+      locations: [],
+      clicked: null,
+    };
+  },
+  mounted() {
+    window.initMap = this.initMap;
+    this.loadData();
+  },
+  methods: {
+    loadData() {
+      /* eslint-disable */
+      const self = this;
+      Papa.parse('https://docs.google.com/spreadsheets/d/1w1wouyCLnPPK8Am0f9HEimZ44t5w9bgnsew4nM-wIS4/pub?output=csv', {
+        download: true,
+        header: true,
+        complete: function(results) {
+          var data = results.data
+          self.locations = data.map((d) => {
+            d.lat = parseFloat(d.lat);
+            d.lng = parseFloat(d.lng);
+            return d;
+          });
+        }
+      })
+      /* eslint-enable */
+    },
+    placeChanged(data) {
+      this.home = data.geometry.location;
+      this.locations.forEach((loc) => {
+        loc.distance = this.getDistance(loc); // eslint-disable-line
+      }, this);
+      this.locations = this.locations.sort((a, b) => a.distance - b.distance);
+    },
+    clearHome() {
+      this.home = null;
+    },
+    getDistance(row) {
+      if (this.home) return this.haversine(row, { lat: this.home.lat(), lng: this.home.lng() });
+      return null;
+    },
+    haversine(point1, point2) {
+      /* eslint-disable */
+      const R = 3958.8; // miles
+      // const R = 6371e3; // metres
+      const φ1 = point1.lat * Math.PI / 180; // φ, λ in radians
+      const φ2 = point2.lat * Math.PI / 180;
+      const Δφ = (point2.lat - point1.lat) * Math.PI / 180;
+      const Δλ = (point2.lng - point1.lng) * Math.PI / 180;
+
+      const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+                Math.cos(φ1) * Math.cos(φ2) *
+                Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+      const d = R * c; // in metres
+      /* eslint-enable */
+      return d.toFixed(2);
+    },
+    showTooltip(loc) {
+      this.clicked = loc;
+    },
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+<style lang="scss">
+#map{
+  height: 200px;
+  width: 60%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.gm-style .gm-style-iw-t:after {
+  background: none !important;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
+import * as VueGoogleMaps from 'vue2-google-maps';
 import Vue from 'vue';
 import App from './App.vue';
 
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.VUE_APP_GOOGLE_API_KEY,
+    libraries: 'places', // necessary for places input
+  },
+});
 Vue.config.productionTip = false;
 
 new Vue({
